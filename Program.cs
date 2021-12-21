eda35bc2e0f01ca3c28c4c1ca4264bc5604119e0
Nash Hymet <foxcorpofficial@gmail.com>
2021-12-20 22:21:43 -0600

    First push!

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Let's print out a nice text
+            Console.WriteLine("Hello World");
+
+            /*
+            I am a block comment and can be used 
+            when you want to add multiple lines 
+            to a comment without having to use
+            / on every line.
+            */
+
+            // Let's create some variables
+            
+        }
+    }
+}
