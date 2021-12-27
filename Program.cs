05be233b51086cd17bdf05d6e64367741d50ffc1
foxcorpofficial <foxcorpofficial@gmail.com>
2021-12-26 20:21:29 -0600

    Good push

--- Program.cs
+++ Program.cs
@@ -17,7 +17,38 @@ namespace HelloWorld
             */
 
             // Let's create some variables
-            
+            string myMessage = "";
+
+            if (args.Length < 1)
+            {
+                myMessage = "Welcome to .NET Core!";
+            }
+            else 
+            {
+                foreach (string item in args)
+                {
+                    myMessage += item;
+                }
+            }
+
+             // Output the myMessage
+            Console.WriteLine(myMessage);
+
+            // Write something to prompt for input
+            Console.WriteLine("What is your name?");
+
+            // Get input
+            var name = Console.ReadLine();
+
+            // The current date
+            var currentDate = DateTime.Now;
+
+            // Do some magic
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+
+
         }
     }
 }
